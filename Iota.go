1e8e6b66eab0e600ef397648f235b39af126aa48
Anshul Kishore <anshulkishore310@gmail.com>
2020-05-03 20:35:33 +0530

    iota example

--- Iota.go
+++ Iota.go
@@ -16,6 +16,12 @@ const (
 	f = iota * 4 //f = 8
 )
 
+const (
+	p = iota //p = 0
+	q        //q = 1
+	r        //r = 2
+)
+
 func main() {
 	fmt.Println("const a : ", a)
 	fmt.Println("const b : ", b)
@@ -30,4 +36,11 @@ func main() {
 	fmt.Printf("%T\n", d)
 	fmt.Printf("%T\n", e)
 	fmt.Printf("%T\n", f)
+
+	fmt.Println("const p : ", p)
+	fmt.Println("const q : ", q)
+	fmt.Println("const r : ", r)
+	fmt.Printf("%T\n", p)
+	fmt.Printf("%T\n", q)
+	fmt.Printf("%T\n", r)
 }
