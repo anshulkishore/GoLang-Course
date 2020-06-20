378844536317d42ce6fbb747ea88ba0347f4a9da
Anshul Kishore <anshulkishore310@gmail.com>
2020-06-20 16:19:02 +0530

    completed ninja 5_3 embedded struct

---
+++ ninja5_3embedded_struct.go
@@ -0,0 +1,43 @@
+package main
+
+import(
+	"fmt"
+)
+
+type vehicle struct {
+	doors int
+	color string
+}
+
+type truck struct {
+	vehicle
+	fourWheel bool
+}
+
+type sedan struct {
+	vehicle
+	luxury bool
+}
+
+func main() {
+	s1 := truck{
+		vehicle: vehicle{
+			doors: 2,
+			color: "black",
+		},
+		fourWheel : true,
+	}
+
+	s2 := sedan{
+		vehicle : vehicle{
+			doors: 4,
+			color: "Silver",
+		},
+		luxury: true,
+	}
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s1.vehicle.doors)
+	fmt.Println(s2.doors)
+}
